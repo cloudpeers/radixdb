09de4d1acf90785dc973a1e37d1ae40fe316e244
Ruediger Klaehn <rklaehn@protonmail.com>
2022-09-29 16:54:45 +0300

    Increase version number and fix filesystem example

--- examples/filesystem.rs
+++ examples/filesystem.rs
@@ -1,8 +1,8 @@
 use radixdb::{radixtree, RadixTree};
 
 fn print_dirs(depth: usize, indent: usize, tree: RadixTree) {
-    for child in tree.group_by(|key, node| key[depth..].iter().any(|x| *x == b'/')) {
-        println!("{:?}", child);
+    for child in tree.group_by(|key, _| key[depth..].iter().any(|x| *x == b'/')) {
+        println!("{}{:?}", " ".repeat(indent), child);
     }
 }
 
